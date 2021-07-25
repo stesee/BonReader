a5674efa787b7a2cb591a809d622d828afd01cf4
Stefan Seeland <168659+stesee@users.noreply.github.com>
2021-07-25 19:32:26 +0200

    Poc - rendering sum

--- reamde.md
+++
@@ -1,7 +0,0 @@
-# Decodes autrian signed invoices
-
-## What helped me to get started
-
-<https://www.w3schools.com/react/react_getstarted.asp>
-<https://www.npmjs.com/package/react-qr-scanner>
-
---
+++ src/QrInvoiceScan.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react'
+import QrReader from 'react-qr-scanner'
+
+class QrInvoiceScan extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            delay: 100,
+            result: 'No result',
+        }
+
+        this.handleScan = this.handleScan.bind(this)
+    }
+    handleScan(data) {
+
+        var concatedInformation = "";
+        var sum = 0;
+        var description = [
+            "Algorithmus & Signaturprovider",
+            "Kassen-ID",
+            "Belegnummer",
+            "Beleg-Datum",
+            "inkl. 20% Steuer",
+            "inkl. 10% Steuer",
+            "inkl. 13% Steuer",
+            "(0% versteuert)",
+            "inkl. 19% Steuer",
+            "Stand-Umsatz-Zaehler-AES256-ICM",
+            "Zertifikat-Seriennummer",
+            "Sig-Voriger-Beleg"];
+
+
+        if (this.isValid(data) === true) {
+            const values = data.text.trim().split('_').splice(1);
+
+            let result = {
+                scanSuccess: true,
+                scanSuccessMessage: "🤑",
+                sum: sum,
+            }
+
+            for (let i = 0; i < values.length; i++) {
+                switch (i) {
+                    case 0:
+                        result.algorytmAndProviderInfo = values[i];
+                        break;
+                    case 1:
+                        result.terminalId = values[i];
+                        break;
+                    case 2:
+                        result.invoiveId = values[i];
+                        break;
+                    case 3:
+                        result.date = values[i];
+                        break;
+                    case 4:
+                        result.twentyPercentTaxSum = values[i];
+                        result.sum = result.sum + parseFloat(values[i].replace(",", "."))
+                        break;
+                    case 5:
+                        result.tenPercentTaxSum = values[i];
+                        result.sum = result.sum + parseFloat(values[i].replace(",", "."))
+                        break;
+                    case 6:
+                        result.thirteenPercentTaxSum = values[i];
+                        result.sum = result.sum + parseFloat(values[i].replace(",", "."))
+                        break;
+                    case 7:
+                        result.zeroPercentTaxSum = values[i];
+                        result.sum = result.sum + parseFloat(values[i].replace(",", "."))
+                        break;
+                    case 8:
+                        result.nineteenPercentTaxSum = values[i];
+                        result.sum = result.sum + parseFloat(values[i].replace(",", "."))
+                        break;
+                    case 9:
+                        result.aes256icm = values[i];
+                        break;
+                    case 10:
+                        result.certificateSerielNumber = values[i];
+                        break;
+                    case 11:
+                        result.signaturePreviousInvoice = values[i];
+                        break;
+
+                    default:
+                        result.uknownProperty = result.uknownProperty + i + " " + values[i] + " ";
+
+                }
+
+            }
+
+            result.sum = result.sum.toLocaleString('de-DE', { minimumFractionDigits: 2 });
+
+            this.setState({ result: result });
+
+            return;
+        }
+        else
+            if (data != null && data.text?.length > 0)
+                this.setState({
+                    result: {
+                        scanSuccess: false,
+                        scanSuccessMessage: "😬 QR-Code lesen fehlgeschlagen." + data
+                    }
+                });
+            else
+                this.setState({
+                    result: {
+                        scanSuccess: false,
+                        scanSuccessMessage: "😑 Kein QR-Code gefunden."
+                    }
+                });
+    }
+
+    isValid(rawCode128) {
+        if (rawCode128 === null)
+            return false;
+
+        //todo: do some more / real validation
+        return rawCode128?.text.includes("_");
+    }
+    handleError(err) {
+        console.error(err)
+    }
+    render() {
+        const previewStyle = {
+            height: 640,
+            width: 480,
+        }
+
+        return (
+            <div>
+                <QrReader
+                    delay={this.state.delay}
+                    style={previewStyle}
+                    onError={this.handleError}
+                    onScan={this.handleScan}
+                />
+                {this.state.result.scanSuccess &&
+                    <p> {this.state.result.scanSuccessMessage}{this.state.result.sum}€</p>
+                }
+                {!this.state.result.scanSuccess &&
+                    <p> {this.state.result.scanSuccessMessage}</p>
+                }
+            </div>
+        )
+    }
+}
+
+export default QrInvoiceScan;
