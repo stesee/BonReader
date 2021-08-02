66cffcb863e29f4e1ea863e57788c284a8b26db8
Stefan Seeland <168659+stesee@users.noreply.github.com>
2021-08-02 19:32:35 +0200

    Change - should be deployed by githubactions

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    Happy scanning!
     <App />
   </React.StrictMode>,
   document.getElementById('root')
