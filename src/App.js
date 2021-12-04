b3b9afaf3128f3e9f7afd81b7f5520d9910ea2b5
Stefan Seeland <168659+stesee@users.noreply.github.com>
2021-12-04 22:01:46 +0100

    Basic feature selecting of camera enabled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import QrInvoiceScan from './QrInvoiceScan';
+import "./App.css";
+import QrInvoiceScan from "./QrInvoiceScan";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <QrInvoiceScan />
+        <QrInvoiceScan selectFacingMode />
       </header>
     </div>
   );
--- src/QrInvoiceScan.js
+++ src/QrInvoiceScan.js
@@ -6,12 +6,51 @@ class QrInvoiceScan extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      delay: 100,
+      cameraId: undefined,
+      delay: 500,
+      devices: [],
+      loading: false,
       result: "No result",
     };
 
     this.handleScan = this.handleScan.bind(this);
   }
+
+  componentWillMount() {
+    const { selectFacingMode } = this.props;
+
+    if (navigator && selectFacingMode) {
+      this.setState({
+        loading: true,
+      });
+
+      navigator.mediaDevices
+        .enumerateDevices()
+        .then((devices) => {
+          const videoSelect = [];
+          devices.forEach((device) => {
+            if (device.kind === "videoinput") {
+              videoSelect.push(device);
+            }
+          });
+          return videoSelect;
+        })
+        .then((devices) => {
+          this.setState({
+            cameraId: devices[0].deviceId,
+            devices,
+            loading: false,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+
+  selectCamera = () => {
+    return this.state.cameraId;
+  };
 
   handleScan(data) {
     const qrCodeParser = new QrCodeParser();
@@ -23,26 +62,64 @@ class QrInvoiceScan extends Component {
   handleError(err) {
     console.error(err);
   }
+
   render() {
+    const { selectFacingMode, selectDelay, legacyMode } = this.props;
+    const { cameraId, devices } = this.state;
+
+    const previewStyle = { width: "100%" };
     return (
       <div>
-        <QrReader
-          delay={this.state.delay}
-          style={{ width: "100%" }}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          legacyMode={true}
-          facingMode={"environment"}
-        />
-        {this.state.result.scanSuccess && (
-          <p>
-            {" "}
-            {this.state.result.scanSuccessMessage}
-            {this.state.result.sum}€
-          </p>
+        {selectFacingMode && devices.length && (
+          <select
+            onChange={(e) => {
+              const value = e.target.value;
+              this.setState({ cameraId: undefined }, () => {
+                this.setState({ cameraId: value });
+              });
+            }}
+          >
+            {devices.map((deviceInfo, index) => (
+              <React.Fragment key={deviceInfo.deviceId}>
+                <option value={deviceInfo.deviceId}>
+                  {deviceInfo.label || `camera ${index}`}
+                </option>
+              </React.Fragment>
+            ))}
+          </select>
         )}
-        {!this.state.result.scanSuccess && (
-          <p> {this.state.result.scanSuccessMessage}</p>
+        {selectDelay && (
+          <div>
+            <button onClick={() => this.setState({ delay: false })}>
+              Disable Delay
+            </button>
+            <input
+              placeholder="Delay in ms"
+              type="number"
+              value={this.state.delay}
+              onChange={(e) =>
+                this.setState({ delay: parseInt(e.target.value) })
+              }
+            />
+          </div>
+        )}
+        {(cameraId || !selectFacingMode) && (
+          <QrReader
+            chooseDeviceId={this.selectCamera}
+            style={previewStyle}
+            onError={this.handleError}
+            onScan={this.handleScan}
+            ref="reader"
+            legacyMode={legacyMode}
+            maxImageSize={1000}
+            delay={this.state.delay}
+            className="reader-container"
+          />
+        )}
+        {legacyMode && (
+          <button onClick={() => this.refs.reader.openImageDialog()}>
+            Open Image Dialog
+          </button>
         )}
       </div>
     );
