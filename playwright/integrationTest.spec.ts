d83a784492d1e7f30cffb3d93afc8832f72f69b5
Stefan Seeland <stefan.seeland@rubicon.eu>
2021-12-07 00:40:32 +0100

    Fixed more warnings

--- playwright/integrationTest.spec.ts
+++ playwright/integrationTest.spec.ts
@@ -1,5 +1,4 @@
-import { test, expect, chromium } from "@playwright/test";
-import { waitFor } from "@testing-library/dom";
+import { test, expect } from "@playwright/test";
 
 // Used hints from https://blog.jarrodwatts.com/how-to-set-up-nextjs-with-jest-react-testing-library-and-playwright to integrate playwright tests into existing project with jstest... main hint was to use the playwright wizard, dont forge to restart vs code and use https://marketplace.visualstudio.com/items?itemName=sakamoto66.vscode-playwright-test-runner
 
